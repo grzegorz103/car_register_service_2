96dcb5d38e1785562ef29e71fe8ca977a0e0de62
Grzesiek <grzesiek555_12@outlook.com>
2019-09-12 19:21:11 +0200

    Update imports

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegisterComponent } from './public/register/register.component';
-import { LoginComponent } from './public/login/login.component';
-import { IndexComponent } from './public/index/index.component';
+import { RegisterComponent } from './shared/register/register.component';
+import { LoginComponent } from './shared/login/login.component';
+import { IndexComponent } from './shared/index/index.component';
 import { CarListComponent } from './car/car-list/car-list.component';
 import { CarAddComponent } from './car/car-add/car-add.component';
 import { CarDetailsComponent } from './car/car-details/car-details.component';
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './public/navbar/navbar.component';
-import { RegisterComponent } from './public/register/register.component';
-import { LoginComponent } from './public/login/login.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { RegisterComponent } from './shared/register/register.component';
+import { LoginComponent } from './shared/login/login.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './services/user.service';
-import { IndexComponent } from './public/index/index.component';
-import { FooterComponent } from './public/footer/footer.component';
 import { HttpRequestInterceptor } from './services/request-interceptor';
 import { CarListComponent } from './car/car-list/car-list.component';
 import { CarAddComponent } from './car/car-add/car-add.component';
 import { CarDetailsComponent } from './car/car-details/car-details.component';
 import { CarRegisterListComponent } from './car-register-list/car-register-list.component';
+import { IndexComponent } from './shared/index/index.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
